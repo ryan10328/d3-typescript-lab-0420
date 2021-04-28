dcfd798ee6dfe621e4b28789ead35eeceffa72c8
ryan.tseng <ryan.tseng@hp.com>
2021-04-28 17:38:50 +0800

    adding new lab into origin code base

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,19 @@ const bars = container.selectAll('.bar')
     .attr('y', data => yScale(data.value));
 
 
-setTimeout(() => {
-    bars.data(dummyData.slice(0, 2)).exit().remove();
-}, 2000);
+setInterval(() => {
+    for (let item of dummyData) {
+        item.value = Math.floor(Math.random() * 15);
+    }
+    bars.data(dummyData).transition().duration(500).ease(d3.easeLinear)
+        .attr('width', xScale.bandwidth())
+        .attr('height', data => 200 - yScale(data.value))
+        .attr('x', data => xScale(data.region))
+        .attr('y', data => yScale(data.value))
+        .attr('fill', getColor);
+}, 1000);
+
+
+function getColor(data: any) {
+    return `rgb(${Math.round(data.value * 17)}, ${Math.round(data.value * 3)}, ${Math.round(data.value * 8)})`;
+}
